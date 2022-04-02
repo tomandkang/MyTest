a687a48edfae337b26c913089368fb1f737f5226
chenkangwen <1805572826@qq.com>
2022-04-02 09:36:17 +0800

    测试类提交

--- src/test/java/com/example/demo/MybatisTest.java
+++ src/test/java/com/example/demo/MybatisTest.java
@@ -1,24 +1,16 @@
 package com.example.demo;
 
-import com.alibaba.fastjson.JSONObject;
 import com.example.demo.dao.IOrderDao;
-import com.example.demo.entity.Commodity;
 import com.example.demo.entity.Order;
-import com.sun.istack.internal.NotNull;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.elasticsearch.action.bulk.BulkRequest;
-import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.common.xcontent.XContentType;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.InputStream;
-import java.lang.reflect.Field;
 
 /**
  * @Description:
@@ -28,72 +20,35 @@ import java.lang.reflect.Field;
 public class MybatisTest extends DemoApplicationTests {
 
     private SqlSessionFactory sqlSessionFactory;
+    private SqlSession sqlSession;
 
-    private static final String AUTO_PUSH_SHIELD_PLATFORM="[\"Amazon\",\"Wish\",\"EBay\",\"Shopee\",\"B2W\",\"Lazada\"]";
 
-    @Autowired
-    private RestHighLevelClient restHighLevelClient;
-
+    //这个SqlSessionFactoryBuilder类的存在很简单,取名也叫他构建器,Mybatis的官网是这样解释它的,
+    //这个类可以被实例化(因为它有且仅有一个默认的无参构造),
+    //使用它的目的就是用来创建多个SqlSessionFactory实例,
+    //最好不要让他一直存在,进而保证所有用来解析xml的资源可以被释放
     @Before
-    public void setUp() throws Exception{
-        ClassPathResource resource = new ClassPathResource("config/mybatis-config.xml");
+    public void setUp() throws Exception {
+        ClassPathResource resource = new ClassPathResource("mybatis-config.xml");
         InputStream inputStream = resource.getInputStream();
         sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
     }
 
     @Test
-    public void selectUserTest(){
-        String orderId="1234";
-        SqlSession sqlSession = sqlSessionFactory.openSession();
+    public void selectUserTest() {
+        String id = "123456";
+        sqlSession = sqlSessionFactory.openSession();
         IOrderDao iOrderDao = sqlSession.getMapper(IOrderDao.class);
-        Order order = iOrderDao.getOrderById(orderId);
+        Order order = iOrderDao.getOrderById(id);
+        System.out.println(order);
+        sqlSession.commit();
         sqlSession.close();
     }
 
-    @Test
-    public void test_1(){
-        Class classes=Order.class;
-        String name = classes.getName();
-        System.out.println(name);
+    @After
+    public void setDown() throws Exception {
+        sqlSession.commit();
+        sqlSession.close();
     }
 
-    //创建文档
-    @Test
-    public void test_2() throws Exception{
-        Commodity commodity = new Commodity();
-        commodity.setCommodityId(3L);
-        commodity.setCommodityName("hahaha");
-        commodity.setPictureUrl("www.google.com");
-        commodity.setPrice(5.20F);
-
-        BulkRequest bulkAddRequest = new BulkRequest();
-        IndexRequest indexRequest =getIndexRequest("commodityId","commodity", "onsale",commodity);
-        bulkAddRequest.add(indexRequest);
-        restHighLevelClient.bulk(bulkAddRequest);
-    }
-
-    @NotNull
-    private IndexRequest getIndexRequest(String primary, String index, String type, Object o) throws Exception {
-        String id = getFieldValueByFieldName(primary, o);
-        String data = null;
-        try {
-            data = JSONObject.toJSONString(o);
-        } catch (Exception e) {
-            throw new Exception(e);
-        }
-        IndexRequest indexRequest = new IndexRequest(index, type, id);
-        indexRequest.source(data, XContentType.JSON);
-        return indexRequest;
-    }
-
-    public static String getFieldValueByFieldName(String fieldName, Object object) {
-        try {
-            Field field = object.getClass().getDeclaredField(fieldName);
-            //对private的属性的访问
-            field.setAccessible(true);
-            return field.get(object) == null ? null : field.get(object).toString();
-        } catch (Exception e) {
-            return null;
-        }
-    }
 }
