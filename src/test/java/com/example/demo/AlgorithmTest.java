5aae450dcf73a2785e83446dae4e0d0fdfef8e79
chenkangwen <1805572826@qq.com>
2022-04-25 10:21:07 +0800

    算法提交

--- src/test/java/com/example/demo/AlgorithmTest.java
+++ src/test/java/com/example/demo/AlgorithmTest.java
@@ -3,6 +3,11 @@ package com.example.demo;
 import com.alibaba.fastjson.JSON;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 public class AlgorithmTest extends DemoApplicationTests {
 
     @Test
@@ -33,7 +38,7 @@ public class AlgorithmTest extends DemoApplicationTests {
         System.out.println(judge);
     }
 
-    public Boolean judge(){
+    public Boolean judge() {
         int[] nums = new int[]{1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
@@ -45,5 +50,139 @@ public class AlgorithmTest extends DemoApplicationTests {
         return false;
     }
 
+    @Test
+    public void test2() {
+        int[] nums = new int[]{4, 1, 2, 1, 2};
+        System.out.println(singleNumber(nums));
+    }
+
+    public int singleNumber(int[] nums) {
+        int reduce = 0;
+        for (int num : nums) {
+            reduce = reduce ^ num;
+        }
+        return reduce;
+    }
+
+    @Test
+    public void test3() {
+        int[] nums1 = new int[]{4, 9, 5};
+        int[] nums2 = new int[]{9, 4, 9, 8, 4};
+        int[] intersect = intersect(nums1, nums2);
+        System.err.println(JSON.toJSONString(intersect));
+    }
+
+    public int[] intersect(int[] nums1, int[] nums2) {
+        // 先对两个数组进行排序
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        List<Integer> list = new ArrayList<>();
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                // 如果i指向的值小于j指向的值，，说明i指向
+                // 的值小了，i往后移一步
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                // 如果i指向的值大于j指向的值，说明j指向的值
+                // 小了，j往后移一步
+                j++;
+            } else {
+                // 如果i和j指向的值相同，说明这两个值是重复的，
+                // 把他加入到集合list中，然后i和j同时都往后移一步
+                list.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        //把list转化为数组
+        int index = 0;
+        int[] res = new int[list.size()];
+        for (int k = 0; k < list.size(); k++) {
+            res[index++] = list.get(k);
+        }
+        return res;
+    }
+
+    public int[] intersectMap(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        ArrayList<Integer> list = new ArrayList<>();
+
+        //先把数组nums1的所有元素都存放到map中，其中key是数组中
+        //的元素，value是这个元素出现在数组中的次数
+        for (int i = 0; i < nums1.length; i++) {
+            map.put(nums1[i], map.getOrDefault(nums1[i], 0) + 1);
+        }
+
+        //然后再遍历nums2数组，查看map中是否包含nums2的元素，如果包含，
+        //就把当前值加入到集合list中，然后再把对应的value值减1。
+        for (int i = 0; i < nums2.length; i++) {
+            if (map.getOrDefault(nums2[i], 0) > 0) {
+                list.add(nums2[i]);
+                map.put(nums2[i], map.get(nums2[i]) - 1);
+            }
+        }
+
+        //把集合list转化为数组
+        int[] res = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+
+    @Test
+    public void test4(){
+        int[] digits=new int[]{4,3,2,1};
+        int[] nums = plusOne(digits);
+        System.out.println(JSON.toJSONString(nums));
+    }
+
+    public int[] plusOne(int[] digits) {
+        int length = digits.length;
+        for (int i = length - 1; i >= 0; i--) {
+            if (digits[i] != 9) {
+                //如果数组当前元素不等于9，直接加1
+                //然后直接返回
+                digits[i]++;
+                return digits;
+            } else {
+                //如果数组当前元素等于9，那么加1之后
+                //肯定会变为0，我们先让他变为0
+                digits[i] = 0;
+            }
+        }
+        //除非数组中的元素都是9，否则不会走到这一步，
+        //如果数组的元素都是9，我们只需要把数组的长度
+        //增加1，并且把数组的第一个元素置为1即可
+        int temp[] = new int[length + 1];
+        temp[0] = 1;
+        return temp;
+    }
+
+    @Test
+    private void test5(){
+        int[] nums =new int[]{0,1,0,3,12};
+
+
+
+    }
+
+
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return;
+        int index = 0;
+        //一次遍历，把非零的都往前挪
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0)
+                nums[index++] = nums[i];
+        }
+        //后面的都是0,
+        while (index < nums.length) {
+            nums[index++] = 0;
+        }
+    }
 
 }
