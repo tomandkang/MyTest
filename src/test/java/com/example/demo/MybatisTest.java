1ddc6c251e4c28fd17755427afa07cb80d17441e
chenkangwen <1805572826@qq.com>
2022-05-25 16:41:31 +0800

    mybatis数据持久化

--- src/test/java/com/example/demo/MybatisTest.java
+++ src/test/java/com/example/demo/MybatisTest.java
@@ -7,10 +7,8 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.jupiter.api.Test;
-import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.Reader;
 
 /**
