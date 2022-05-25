ebd661d3cdaeed9445725c3819f2535ed9dc3609
chenkangwen <1805572826@qq.com>
2022-05-25 16:13:19 +0800

    mybatis数据持久化

--- src/test/java/com/example/demo/MybatisTest.java
+++ src/test/java/com/example/demo/MybatisTest.java
@@ -5,8 +5,6 @@ import com.example.demo.entity.Order;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
 
@@ -27,28 +25,18 @@ public class MybatisTest extends DemoApplicationTests {
     //这个类可以被实例化(因为它有且仅有一个默认的无参构造),
     //使用它的目的就是用来创建多个SqlSessionFactory实例,
     //最好不要让他一直存在,进而保证所有用来解析xml的资源可以被释放
-    @Before
-    public void setUp() throws Exception {
-        ClassPathResource resource = new ClassPathResource("mybatis-config.xml");
+    @Test
+    public void selectUserTest() throws Exception {
+        String id = "123456";
+        ClassPathResource resource = new ClassPathResource("/config/mybatis-config.xml");
         InputStream inputStream = resource.getInputStream();
         sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-    }
-
-    @Test
-    public void selectUserTest() {
-        String id = "123456";
         sqlSession = sqlSessionFactory.openSession();
         IOrderDao iOrderDao = sqlSession.getMapper(IOrderDao.class);
         Order order = iOrderDao.getOrderById(id);
-        System.out.println(order);
         sqlSession.commit();
         sqlSession.close();
     }
 
-    @After
-    public void setDown() throws Exception {
-        sqlSession.commit();
-        sqlSession.close();
-    }
 
 }
