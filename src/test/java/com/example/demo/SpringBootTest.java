dc07dffd7c86f0afe3830d5f2ab249acf6759c18
chenkangwen <1805572826@qq.com>
2022-08-22 14:32:06 +0800

    mybatis数据持久化

--- src/test/java/com/example/demo/SpringBootTest.java
+++ src/test/java/com/example/demo/SpringBootTest.java
@@ -7,6 +7,11 @@ import com.example.demo.entity.TestD;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 public class SpringBootTest extends DemoApplicationTests {
 
     @Test
@@ -57,4 +62,25 @@ public class SpringBootTest extends DemoApplicationTests {
     public void testA(){
         testA.printName();
     }
+
+    @Test
+    public void test1314(){
+       // String[] split = "22/08/15".split("\\/");
+       // System.out.println(split[0]+"-"+split[1]+"-"+split[2]);
+        Calendar calendar = Calendar.getInstance();//日历对象
+        calendar.setTime(new Date());//设置当前日期
+        String yearStr = calendar.get(Calendar.YEAR) + "";//获取年份
+        String yearPrefix = yearStr.substring(0,2);
+        System.out.println(yearPrefix);
+        //System.out.println(yearStr);
+    }
+    @Test
+    public void test13145(){
+        List<String> list= new ArrayList<>();
+        list.add("hehe");
+        list.add("haihai");
+        System.out.println(String.join(",",list));
+    }
+
+
 }
