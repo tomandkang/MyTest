a73d0a5bd12a1d4e86696f2109b15ea3f8ba2e71
chenkangwen <1805572826@qq.com>
2022-08-31 16:27:05 +0800

    mybatis数据持久化

--- src/test/java/com/example/demo/ThreadTest.java
+++ src/test/java/com/example/demo/ThreadTest.java
@@ -1,21 +1,29 @@
 package com.example.demo;
 
 
+import com.alibaba.fastjson.JSON;
 import com.example.demo.commom.threads.MyThreadOne;
 import com.example.demo.commom.threads.MyThreadThree;
 import com.example.demo.commom.threads.MyThreadTwo;
 import com.example.demo.entity.Father;
 import com.example.demo.entity.Son;
-import com.sun.org.apache.bcel.internal.generic.NEW;
-import org.junit.jupiter.api.Test;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.stream.Collectors;
 
 public class ThreadTest extends DemoApplicationTests {
 
+    private static final Random random = new Random();
 
     /**
      * 线程的五种状态: 新建 就绪 运行 阻塞 死亡
@@ -67,4 +75,34 @@ public class ThreadTest extends DemoApplicationTests {
         Father son = new Son();
         son.say();
     }
+
+    @Test
+    public void test5() {
+        List<String> sellPoints = new ArrayList<>();
+        sellPoints.add("");
+        sellPoints.add("123456789");
+        sellPoints.add("qwer");
+        sellPoints = sellPoints.stream().filter(StringUtils::isNotEmpty).collect(Collectors.toList());
+        System.out.println(JSON.toJSONString(sellPoints));
+    }
+
+    @Test
+    public void test6() {
+        Map<String, String> map = new HashMap<>();
+        map.put("haha", "haha");
+        map.put("haha", "xixi");
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + "---" + entry.getValue());
+        }
+    }
+
+    @Test
+    public void test7() {
+        while (true) {
+            int i = random.nextInt(5);
+            System.out.println(i);
+        }
+    }
+
+
 }
