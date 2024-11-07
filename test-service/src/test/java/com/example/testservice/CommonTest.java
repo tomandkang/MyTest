3bbda49fb411c5a8698c8ad992f19b5e437402c7
chenkangwen <1805572826@qq.com>
2024-11-07 15:26:15 +0800

    增加common模块

--- test-service/src/test/java/com/example/testservice/CommonTest.java
+++ test-service/src/test/java/com/example/testservice/CommonTest.java
@@ -44,6 +44,10 @@ public class CommonTest {
     private RestHighLevelClient restHighLevelClient;
 
     public static void main(String[] args) {
+
+        Integer reverse = reverse(50);
+        System.out.println(reverse);
+
         if (Objects.nonNull(args)) {
             for (int i = 0; i < args.length; i++) {
                 System.out.println(args[i]);
