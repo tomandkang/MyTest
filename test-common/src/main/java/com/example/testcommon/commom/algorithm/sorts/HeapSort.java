468345dbb3995fd4513259842ff93f7b454eeea5
chenkangwen <1805572826@qq.com>
2024-11-27 15:40:58 +0800

    1
    1
    
    1

--- test-common/src/main/java/com/example/testcommon/commom/algorithm/sorts/HeapSort.java
+++ test-common/src/main/java/com/example/testcommon/commom/algorithm/sorts/HeapSort.java
@@ -3,7 +3,14 @@ package com.example.testcommon.commom.algorithm.sorts;
 public class HeapSort {
 
 
+    /**
+     * @description: 堆排序算法
+     * @author: chenkangwen
+     * @date: 2024/11/27
+     * @param: [array]
+     */
     public static void heapSort(int[] array) {
+
         for (int i = array.length / 2 - 1; i >= 0; i--) {
 
             adjustHeap(array, i, array.length);
@@ -24,20 +31,24 @@ public class HeapSort {
      * @param: [array, index, length]
      */
     public static void adjustHeap(int[] array, int i, int length) {
+
         int temp = array[i];
 
-        for (int k = 2 * i + 1; k < length; k = 2 * i + 1) {
+        for (int k = 2 * i + 1; k < length; k = 2 * k + 1) {
 
             if (k + 1 < length && array[k] < array[k + 1]) {
 
                 k++;
             }
             if (array[k] <= temp) {
+
                 break;
             }
             array[i] = array[k];
+
             i = k;
         }
+
         array[i] = temp;
     }
 
--- test-common/src/main/java/com/example/testcommon/commom/algorithm/sorts/InsertionSort.java
+++ test-common/src/main/java/com/example/testcommon/commom/algorithm/sorts/InsertionSort.java
@@ -3,6 +3,12 @@ package com.example.testcommon.commom.algorithm.sorts;
 public class InsertionSort {
 
 
+    /**
+     * @description: 插入排序法
+     * @author: chenkangwen
+     * @date: 2024/11/27
+     * @param: [nums]
+     */
     public static void insertionSort(int[] nums) {
         int temp, j;
         for (int i = 1; i < nums.length; i++) {
