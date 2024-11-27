bc39de86a43c7ae90ab8a2319691fffbd72698c7
chenkangwen <1805572826@qq.com>
2024-11-27 15:40:58 +0800

    1

--- test-common/src/main/java/com/example/testcommon/commom/algorithm/sorts/HeapSort.java
+++ test-common/src/main/java/com/example/testcommon/commom/algorithm/sorts/HeapSort.java
@@ -4,6 +4,7 @@ public class HeapSort {
 
 
     public static void heapSort(int[] array) {
+
         for (int i = array.length / 2 - 1; i >= 0; i--) {
 
             adjustHeap(array, i, array.length);
@@ -24,20 +25,24 @@ public class HeapSort {
      * @param: [array, index, length]
      */
     public static void adjustHeap(int[] array, int i, int length) {
+
         int temp = array[i];
 
-        for (int k = 2 * i + 1; k < length; k = 2 * i + 1) {
+        for (int k = 2 * i + 1; k < length; k = 2 * k + 1) {
 
             if (k + 1 < length && array[k] < array[k + 1]) {
 
                 k++;
             }
             if (array[k] <= temp) {
+
                 break;
             }
             array[i] = array[k];
+
             i = k;
         }
+
         array[i] = temp;
     }
 
