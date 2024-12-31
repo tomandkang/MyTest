30f5520112f55cd63d82ad40a050120ddc7a923e
chenkangwen <1805572826@qq.com>
2024-12-31 15:26:55 +0800

    shujuku

--- test-service/src/main/java/com/example/testservice/boot/TestServiceApplication.java
+++ test-service/src/main/java/com/example/testservice/boot/TestServiceApplication.java
@@ -1,5 +1,7 @@
 package com.example.testservice.boot;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -8,6 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = "com.example")
 @ComponentScan(basePackages = "com.example")
+@MapperScan(value = "com.example.testservice.dao", annotationClass = Mapper.class)
 public class TestServiceApplication {
 
     private static Logger logger = LoggerFactory.getLogger(TestServiceApplication.class);
